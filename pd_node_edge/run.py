c9431fe4d2358b56e2a0335d5cd201f976a176c3
hxt-tg <782920705@qq.com>
2021-06-18 19:34:19 +0800

    feat: Add video render to pd_node_edge.

--- pd_node_edge/run.py
+++ pd_node_edge/run.py
@@ -1,10 +1,12 @@
 from os.path import join
+import cv2
 import os
 import sys
 import traceback
 from openpyxl import Workbook
 from itertools import product
 import numpy as np
+import matplotlib.pyplot as plt
 
 sys.path.append('..')
 from utils import option_choice, init, select_data_file, load_session, grid
@@ -29,7 +31,7 @@ def stats_strategy_response(data: OTreeSessionData, ws=None):
     for rnd, (pr, ne) in enumerate(zip(rounds[:-1], rounds[1:]), 2):
         stats = {k1: {k2: 0 for k2 in product(('Node', 'Edge'), repeat=2)}
                  for k1 in product(list('CD'), repeat=2)}
-        print(f'\rCalculating strategy response (round {rnd}) ...    ', end='')
+        print(f'\rCalculating strategy response (round {rnd}) ... ', end='')
         for pid in range(1, ne.num_players() + 1):
             nep = ne.loc[pid]
             prp = pr.loc[pid]
@@ -98,21 +100,22 @@ def save_strategy_response(session_code, data: OTreeSessionData, w, h):
     wb.save(file_name)
 
 
+STRATEGY_RESPONSE_COLOR_MAP = [
+    '#733d11', '#a65819', '#d97321', '#ff8826',
+    '#5a7328', '#82a63a', '#aad94c', '#c8ff59',
+    '#116473', '#1991a6', '#21bdd9', '#26deff',
+    '#5c1773', '#8521a6', '#ad2bd9', '#cc33ff',
+]
+
+
 def save_strategy_response_barplot(session_code, data: OTreeSessionData, w, h):
     """Save a bar plot of strategy response statistics."""
-    import matplotlib.pyplot as plt
-
     file_name = join(OUTPUT_DIR, f'{APP_NAME}_response_stats_{session_code}.pdf')
     rnd_stats = global_vars.get('rnd_stats', None)
     if rnd_stats is None: rnd_stats = stats_strategy_response(data)
 
-    colors = [
-        '#733d11', '#a65819', '#d97321', '#ff8826',
-        '#5a7328', '#82a63a', '#aad94c', '#c8ff59',
-        '#116473', '#1991a6', '#21bdd9', '#26deff',
-        '#5c1773', '#8521a6', '#ad2bd9', '#cc33ff', ]
     ratio = []
-    rounds = list(range(2, 51))
+    rounds = list(range(2, data.num_rounds() + 1))
     for rnd, stats in rnd_stats.items():
         values = sum(map(lambda s: list(s.values()), stats.values()), [])
         ratio.append([v / sum(values) for v in values])
@@ -122,13 +125,95 @@ def save_strategy_response_barplot(session_code, data: OTreeSessionData, w, h):
     start = np.zeros(ratio[0].shape)
     for i, r in enumerate(ratio):
         if sum(r) < 1e-6: continue
-        ax.bar(rounds, r, bottom=start, width=1, color=colors[i])
+        ax.bar(rounds, r, bottom=start, width=1, color=STRATEGY_RESPONSE_COLOR_MAP[i])
         start += r
     ax.axis([min(rounds) - 0.5, max(rounds) + 0.5, 0, 1])
     ax.set_xlabel('Rounds')
     ax.set_ylabel('Ratio')
-    plt.savefig(file_name)
-    plt.show()
+    fig.savefig(file_name)
+    fig.show()
+
+
+VIDEO_COLOR_MAP = dict(
+    Node=dict(D='#6A8FE6', C='#EA7462'),
+    Edge=dict(D='#1C47AD', C='#B32A15')
+)
+
+
+def round_plot(data, file_name, rnd, w, h):
+    plt.close('all')
+    fig, ax = plt.subplots()
+
+    def _fill_xy(i, j, _type):
+        points = [[i - 0.5, j - 0.5], [i - 0.5, j + 0.5], [i + 0.5, j + 0.5], [i + 0.5, j - 0.5], [i - 0.5, j - 0.5]]
+        if _type == 'Node':
+            return np.array(points[:-1])
+        else:
+            return dict(
+                list(zip(list('LURD'), [np.array([[i, j]] + [p1, p2]) for p1, p2 in zip(points[:-1], points[1:])])))
+
+    def _plot_player(i, j, d):
+        if d['type'] == 'Node':
+            xy = _fill_xy(i, j, d['type'])
+            ax.fill(xy[:, 0], xy[:, 1], VIDEO_COLOR_MAP[d['type']][d['L']])
+        else:
+            for di, xy in _fill_xy(i, j, d['type']).items():
+                ax.fill(xy[:, 0], xy[:, 1], VIDEO_COLOR_MAP[d['type']][d[di]])
+
+    for pid, p_data in data.items():
+        pi = (pid - 1) // w + 1
+        pj = (pid - 1) % w + 1
+        _plot_player(pi, pj, p_data)
+    ax.set_aspect('equal')
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    ax.set_title(f'Round {rnd} (Red for cooperation, blue for defection)')
+    ax.axis('off')
+    fig.tight_layout()
+    fig.savefig(file_name, dpi=200)
+
+
+def release_video(file_name, image_name_list, fps=24, video_type='mp4'):
+    assert len(image_name_list) > 0, "No images found for rendering."
+    image = cv2.imread(image_name_list[0])
+    height, width, _ = image.shape
+    codecs = {
+        'mp4': cv2.VideoWriter_fourcc(*'MP4V'),
+        'avi': cv2.VideoWriter_fourcc(*'DIVX'),
+    }
+    assert video_type.lower() in codecs, 'Error video type.'
+
+    video = cv2.VideoWriter(file_name, codecs[video_type], float(fps),
+                            (width, height))
+    for i, image_name in enumerate(image_name_list, 1):
+        print(f'\rRelease video [Round {i}] ... ', end='')
+        for _ in range(int(fps)):
+            image = cv2.imread(image_name)
+            video.write(image)
+    video.release()
+    cv2.destroyAllWindows()
+    print('Done')
+
+
+def render_video(session_code, data: OTreeSessionData, w, h):
+    """Save a video for this session."""
+    dir_name = join(OUTPUT_DIR, f'{APP_NAME}_render_{session_code}')
+    video_name = join(dir_name, 'output.mp4')
+    if not os.path.exists(dir_name): os.mkdir(dir_name)
+    image_name_list = []
+    for rnd in range(1, data.num_rounds() + 1):
+        fig_name = join(dir_name, f'Round_{rnd}.png')
+        print(f'\rRender image for round {rnd} ... ', end='')
+        round_data = data.get_round(rnd)
+        round_plot_data = dict()
+        for pid in range(1, round_data.num_players() + 1):
+            d = round_data.loc[pid]
+            round_plot_data[pid] = dict(type=d.player.type, L=d.player.choice_L, U=d.player.choice_U,
+                                        R=d.player.choice_R, D=d.player.choice_D)
+        round_plot(round_plot_data, fig_name, rnd, w, h)
+        image_name_list.append(fig_name)
+    print('Done')
+    release_video(video_name, image_name_list, video_type='mp4')
 
 
 def fill_edge_choice(data: OTreeSessionData):
@@ -152,7 +237,7 @@ def calculate_data(data: OTreeSessionData, w, h):
                                            'choice_nei_L choice_nei_U choice_nei_R choice_nei_D'.split(' '))))
     for idx, row in data.iterrows():
         rnd, pid = idx
-        print(f'\rProducing neighbor info (round {rnd}, player {pid}) ...    ', end='')
+        print(f'\rProducing neighbor info (round {rnd}, player {pid}) ... ', end='')
         data.loc[idx, ('player', 'pid_L')] = nei[pid]['L']
         data.loc[idx, ('player', 'pid_U')] = nei[pid]['U']
         data.loc[idx, ('player', 'pid_R')] = nei[pid]['R']
@@ -173,6 +258,7 @@ OPERATION = [
     save_neighbor_info,
     save_strategy_response,
     save_strategy_response_barplot,
+    render_video,
 ]
 
 
